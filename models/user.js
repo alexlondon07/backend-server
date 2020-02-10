58d67551223211baa7bd1e3546e3bfab1865f8d4
Alexander Andres Londoño Espejo <alexander.a.londono@accenture.com>
2020-02-10 10:32:08 -0500

    Creando el modelo de usuarios - Node

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     name: { type: String , required: [true, 'El nombre es requerido'] },
-    email: { type: String, uniuqe:true, required: [true, 'El email es requerido'] },
+    email: { type: String, unique:true, required: [true, 'El email es requerido'] },
     password: { type: String, required: [true, 'La contraseña es requerida'] },
     img: { type: String, required: false },
     role: { type: String, required: true, default: 'USER_ROLE' },
